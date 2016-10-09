85a3c3cf87fef0d9e4458bcb4b1fe00607221125
arlecaste23 <arlecaste@live.it>
2016-10-09 15:16:22 +0200

    Update presentazione_script.js

--- presentazione_script.js
+++ presentazione_script.js
@@ -1,7 +1,5 @@
 
 var element;
-function fai(id) {
-  element = document.getElementById(id);
-  var text = document.createTextNode("Bella rega");;
-  element.appendChild(text);
+function fai() {
+  document.write("bella rega");
 }
